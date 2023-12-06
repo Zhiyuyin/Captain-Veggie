b7acd8d65f75266d82494bac6340c3e02d4fb977
Zhiyuyin <zyin4@stevens.edu>
2023-12-06 16:47:27 -0500

    feat: add Veggie class

---
+++ Veggie.cpp
@@ -0,0 +1,25 @@
+// Zhiyu Yin, 12/6/2023
+// implementation of Veggie class
+
+#include "Veggie.h"
+#include "FieldInhabitant.h"
+
+#include <ostream>
+#include <string>
+
+Veggie::Veggie(const string &Symbol_, const string &Name_, const int Point_)
+        : FieldInhabitant(Symbol_), veggieName(Name_), veggiePoint(Point_) {};
+
+void Veggie::setVeggieName(const string &name) { veggieName = name; }
+
+void Veggie::setVeggiePoint(const int Point) { veggiePoint = Point; }
+
+const string &Veggie::getVeggieName() const { return veggieName; }
+
+int Veggie::getVeggiePoint() const { return veggiePoint; }
+
+std::ostream &operator<<(std::ostream &os, const Veggie &thisVeggie) {
+    os << thisVeggie.symbol << ": " << thisVeggie.veggieName << " "
+       << thisVeggie.veggiePoint << " points\n";
+    return os;
+}
---
+++ Veggie.h
@@ -0,0 +1,29 @@
+// Zhiyu Yin, 12/6/2023
+// declarations of Veggie class
+
+#pragma once
+
+#include "FieldInhabitant.h"
+#include <iostream>
+#include <ostream>
+#include <string>
+
+class Veggie : public FieldInhabitant {
+private:
+    string veggieName;
+    int veggiePoint;
+
+public:
+    Veggie(const string &, const string &, const int);
+
+    void setVeggieName(const string &);
+
+    void setVeggiePoint(const int);
+
+    const string &getVeggieName() const;
+
+    int getVeggiePoint() const;
+
+    // helper function for checking out current vegetables
+    friend std::ostream &operator<<(std::ostream &, const Veggie &);
+};
